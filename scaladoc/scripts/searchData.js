43d681302b4703e29ea10d71770a50602f32fb53
GitHub Action <action@github.com>
2024-10-05 12:29:38 +0000

    Update ScalaDoc on Jekyll site

--- scaladoc/scripts/searchData.js
+++ scaladoc/scripts/searchData.js
@@ -1,5 +1,6 @@
 pages = [{"l":"index.html#","e":false,"i":"","n":"Scothello","t":"Scothello","d":"","k":"static","x":""},
 {"l":"scothello.html#","e":false,"i":"","n":"scothello","t":"scothello","d":"","k":"package","x":""},
 {"l":"scothello.html#dummyFunction-ffffff27","e":false,"i":"","n":"dummyFunction","t":"dummyFunction(): Int","d":"scothello","k":"def","x":""},
+{"l":"scothello.html#dummyFunction2-665","e":false,"i":"","n":"dummyFunction2","t":"dummyFunction2(): String","d":"scothello","k":"def","x":""},
 {"l":"scothello.html#hello-94c","e":false,"i":"","n":"hello","t":"hello(): Unit","d":"scothello","k":"def","x":""},
 {"l":"scothello/hello.html#","e":false,"i":"","n":"hello","t":"hello","d":"scothello","k":"class","x":""}];
