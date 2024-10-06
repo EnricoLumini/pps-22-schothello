afc038df4af0c3446a19798ffcf4be03a238fc00
Enrico Lumini <enrico.lumini@studio.unibo.it>
2024-10-06 12:22:58 +0200

    Add a dummy test to check if workflow is working

--- src/test/scala/scothello/TestMain.scala
+++ src/test/scala/scothello/TestMain.scala
@@ -6,3 +6,6 @@ class TestMain extends AnyFlatSpec:
 
   "An dummyFunction" should "return 1" in:
     assert(dummyFunction() == 1)
+
+  "And dummyFunction2" should "return 2" in:
+    assert(dummyFunction2() == 2)
