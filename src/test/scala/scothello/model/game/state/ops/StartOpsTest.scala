b9ac468c95a2323dac28e34cfd2d2d96ae0890a9
Enrico Lumini <enrico.lumini@studio.unibo.it>
2024-10-24 20:41:32 +0200

    Add tests for StartOps

--- src/test/scala/scothello/model/game/state/ops/StartOpsTest.scala
+++ src/test/scala/scothello/model/game/state/ops/StartOpsTest.scala
@@ -1,7 +1,22 @@
 package scothello.model.game.state.ops
 
 import scothello.BaseTest
+import scothello.PlayerProvider
+import scothello.model.components.Scores
+import scothello.model.game.Turn
+import scothello.model.game.state.GameState
+import scothello.model.game.state.ops.StartOps.startGame
 
-class StartOpsTest extends BaseTest:
+class StartOpsTest extends BaseTest with PlayerProvider:
 
-  "A State with .." should "have everything none" in {}
+  "A state with Start Ops" should "start the game" in {
+    val state = GameState()
+    val newState = state.startGame(twoPlayers)
+    newState should not be None
+    newState match
+      case Some(s) =>
+        s.players should be(Some(twoPlayers))
+        s.turn shouldBe Turn.initial(twoPlayers._1)
+        s.playerScores shouldBe Scores.initialize(twoPlayers)
+      case None => fail("Expected a new state")
+  }
